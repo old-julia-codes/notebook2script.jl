9db972d3df3619059d613d67ee1ff4a2d66cc03c
SubhadityaMukherjee <msubhaditya@gmail.com>
2020-06-19 20:05:21 +0400

    added start

---
+++ src/notebook2script.jl
@@ -0,0 +1,26 @@
+#using JSON
+
+function csave()
+dict2 = Dict()
+open(ARGS[1], "r") do f
+    global dict2
+    dict2=JSON.parse(f)  
+end
+
+gstr = ""
+for a in dict2["cells"]
+#     @info a["source"]
+    #global gstr
+    if "#export\n" in a["source"]
+#         @info a["source"]
+        temp = a["source"]
+        temp = filter!(e->e≠"#export\n",temp)
+        gstr*= join(temp)
+        gstr*="\n"
+    end
+end
+
+io = open(ARGS[2], "w+")
+write(io, gstr)
+close(io)
+end
